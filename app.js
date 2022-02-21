2a1fbcaf370933ab4d0de063bef600ade0757755
saltyposaune <ewalkertbone@gmail.com>
2022-02-21 16:43:37 -0700

    additional dependencies

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 var app = express();
 import fetch from 'node-fetch';
+var bodyParser = require('body-parser');
+const axios = require('axios');
 var PORT = process.env.PORT || 3002
 
 // app.use(require('./routes'));
+app.use(bodyParser.json());
 
 fetch('https://api.hatchways.io/assessment/blog/posts')
 .then(data => {
