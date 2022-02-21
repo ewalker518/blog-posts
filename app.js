2f5e437b5490bc2a9e4d7a16168d59c556d192f6
saltyposaune <ewalkertbone@gmail.com>
2022-02-21 16:21:50 -0700

    debugged api call

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var express = require('express');
+import express from 'express';
 var app = express();
-var PORT = process.env.PORT || 3000
+import fetch from 'node-fetch';
+var PORT = process.env.PORT || 3002
 
 // app.use(require('./routes'));
 
 fetch('https://api.hatchways.io/assessment/blog/posts')
-    .then(response => {
-        return response.json();
-    })
-    .then(posts => {
-        console.log(posts)
-    });
+.then(data => {
+return data.json();
+})
+.then(post => {
+console.log(post);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port http://localhost:${PORT}`);
